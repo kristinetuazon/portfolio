3c1f8a1923c92a619f5afca27099d7a9d3ec6a95
kristinetuazon <kristine.jane.tuazon@gmail.com>
2023-01-02 21:03:13 +0900

    feat: projects page

--- portfolio/src/components/NavBar.tsx
+++ portfolio/src/components/NavBar.tsx
@@ -3,12 +3,12 @@ import React, { useEffect, useState, type ReactNode } from "react";
 
 const NavBar = () => {
   return (<>
-  <nav className="flex fixed bottom-0 inset-x-0 z-999999 w-100 justify-center items-center">
-  <div className="grid grid-cols-4 gap-4">
-  <a className="text-white font-audreyMedium uppercase" href="#name">Home</a>
+  <nav className="flex fixed bottom-0 inset-x-0 z-999999 w-100 justify-evenly ">
+  <div className="grid grid-cols-4 gap-4 ">
+  <a className="text-white font-audreyMedium inset-y-0 left-0 uppercase" href="#name">Start</a>
   <a className="text-white font-audreyMedium uppercase" href="#about">About</a>
-  <a className="text-white font-audreyMedium uppercase" href="#projects">Projects</a>
-  <a className="text-white font-audreyMedium uppercase" href="#contact">Contact</a>
+  <a className="text-white font-audreyMedium uppercase" href="#projects">Project</a>
+  <a className="text-white font-audreyMedium uppercase inset-0 " href="#contact">Contact</a>
   </div>
 </nav>
   </>)
