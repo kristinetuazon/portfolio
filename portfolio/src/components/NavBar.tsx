6349cc61ce6ce70f1754a754e7f8ae5b3e532b22
kristinetuazon <kristine.jane.tuazon@gmail.com>
2023-02-10 12:32:13 +0900

    fix: change the position of nav bar & add moon

--- portfolio/src/components/NavBar.tsx
+++ portfolio/src/components/NavBar.tsx
@@ -72,36 +72,41 @@ const NavBar = () => {
 
   return (
     <>
-      <nav className="z- w-100 fixed inset-x-0 bottom-0 flex justify-evenly ">
+      <nav className="w-100 fixed inset-x-0 top-5 flex justify-evenly ">
         <div className="grid grid-cols-4 gap-4 ">
-          <a
-            id="navStart"
-            className="inset-y-0 left-0 font-audreyMedium uppercase text-white"
-            href="#name"
-          >
-            Start
-          </a>
-          <a
-            id="navAbout"
-            className="font-audreyMedium uppercase text-white"
-            href="#about"
-          >
-            About
-          </a>
-          <a
-            id="navProject"
-            className="font-audreyMedium uppercase text-white"
-            href="#projects"
-          >
-            Project
-          </a>
-          <a
-            id="navContact"
-            className="inset-0 font-audreyMedium uppercase text-white "
-            href="#contact"
-          >
-            Contact
-          </a>
+          <div id="navStart" className="grid grid-rows-2 place-items-center">
+            <p>🌑</p>
+            <a
+              className="inset-y-0 left-0 font-audreyMedium uppercase text-white"
+              href="#name"
+            >
+              Start
+            </a>
+          </div>
+          <div id="navAbout" className="grid grid-rows-2 place-items-center">
+            <p>🌘</p>
+            <a className="font-audreyMedium uppercase text-white" href="#about">
+              About
+            </a>
+          </div>
+          <div id="navProject" className="grid grid-rows-2 place-items-center">
+            <p>🌖</p>
+            <a
+              className="font-audreyMedium uppercase text-white"
+              href="#projects"
+            >
+              Project
+            </a>
+          </div>
+          <div id="navContact" className="grid grid-rows-2 place-items-center">
+            <p>🌕</p>
+            <a
+              className="inset-0 font-audreyMedium uppercase text-white "
+              href="#contact"
+            >
+              Contact
+            </a>
+          </div>
         </div>
       </nav>
     </>
